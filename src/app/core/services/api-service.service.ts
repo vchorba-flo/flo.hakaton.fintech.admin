6fbb27dda17641caa5800e869c454f3fa82e18bb
vstepanov-flo <vstepanov@flo.team>
2022-07-09 00:13:09 +0300

    Add api url for api service

--- src/app/core/services/api-service.service.ts
+++ src/app/core/services/api-service.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of, shareReplay } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface ShopStatistic {
+  name: string;
+  totalBankProfit: number;
+  purchasesNumber: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getShopsStatistics(): Observable<ShopStatistic[]> {
+    return of([
+      { name: 'Магнит', totalBankProfit: 100000, purchasesNumber: 52456 },
+      { name: 'Пятёрочка', totalBankProfit: 200000, purchasesNumber: 22456 },
+      { name: 'Перекрёсток', totalBankProfit: 300000, purchasesNumber: 32456 },
+    ])
+    // return this.httpClient.get(`${this.apiUrl}statistics/shop`)
+    //   .pipe(
+    //     shareReplay(1)
+    //   );
+  }
 }
